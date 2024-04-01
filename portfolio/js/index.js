f664ae9bbd19c97ef8e0d3f1ec6defee5839091a
Robbe Magerman <155002975+Robbe04@users.noreply.github.com>
2024-04-01 19:10:05 +0200

    Add files via upload

---
+++ portfolio/js/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Naam ingeven gebruiker
+// const start = function () {
+//   const gebruikersNaam = alert(prompt("Hallo gebruiker, wat is je naam? "));
+//   document.getElementById("naamGebruiker").textContent = `${gebruikersNaam}`;
+// };
+
+// const buttonLogin = function () {
+//    const test = "Robbe";
+//   const gebruikersNaam = document
+//     .getElementById("buttonGebruiker")
+//     .onclick(() => {
+//       document.getElementById("naamGebruiker") = `${test}`;
+//     });
+// };
